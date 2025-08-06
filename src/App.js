d75a6298609f40d084faca245852126c7950fc1e
DAWIT2394 <davebireda2394@gmail.com>
2025-08-06 17:27:41 +0300

    perfect

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ButcherPage from './components/ButcherPage';
 import Adminpage from './components/Adminpage';
 import ReportPage from './components/ReportPage';
-import Home from './components/home';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Router>
         <Routes>
-        <Route path="/Home" element={<Home />} />
 
           <Route path="/" element={<ButcherPage />} />
           <Route path="/admin" element={<Adminpage />} />
---
+++ src/components/CookerPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CookerPage() {
+  return (
+    <div>
+      <h1>CookerPage</h1>
+    </div>
+  )
+}
